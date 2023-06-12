0ef8cab36606aa0cd6b655ee0126896b73f3151f
egoavara <egoavara@gmail.com>
2023-06-13 00:22:27 +0900

    fix: 한줄 표현식이 안되나봄

--- .deploy/variable.tf
+++ .deploy/variable.tf
@@ -2,7 +2,10 @@ variable "gcp_project" { type = string }
 variable "gcp_region" { type = string }
 variable "gcp_zone" { type = string }
 
-variable "kubernetes_config_path" { type = string, default = "~/.kube/config" }
+variable "kubernetes_config_path" {
+  type    = string
+  default = "~/.kube/config"
+}
 variable "kubernetes_config_context" { type = string }
 
 variable "keycloak_admin_username" { type = string }
